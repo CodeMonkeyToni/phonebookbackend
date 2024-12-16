a8456802ca2ef69ecc6f1a24a1a91024cefc0eff
CodeMonkeyToni <toni.koljonen@gmail.com>
2024-12-16 17:34:47 +0200

    CSP testing again

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
 
-app.use((req, res, next) => {
-    res.setHeader('Content-Security-Policy', 
-        "default-src 'none'; " +  // Estä kaikki lähteet oletuksena
-        "script-src 'self' 'unsafe-inline'; " + // Salli skriptit vain omasta lähteestä ja inline-skriptit
-        "style-src 'self' 'unsafe-inline'; " +  // Salli tyylit vain omasta lähteestä ja inline-tyylit
-        "img-src 'self'; " +  // Salli kuvat vain omasta lähteestä
-        "font-src 'self'; " + // Salli fontit vain omasta lähteestä
-        "connect-src 'self'; " + // Salli yhteydet omasta lähteestä
-        "worker-src 'self';");  // Salli työntekijät omasta lähteestä
-    next();
-})
+// app.use((req, res, next) => {
+//     res.setHeader('Content-Security-Policy', 
+//         "default-src 'none'; " +  // Estä kaikki lähteet oletuksena
+//         "script-src 'self' 'unsafe-inline'; " + // Salli skriptit vain omasta lähteestä ja inline-skriptit
+//         "style-src 'self' 'unsafe-inline'; " +  // Salli tyylit vain omasta lähteestä ja inline-tyylit
+//         "img-src 'self'; " +  // Salli kuvat vain omasta lähteestä
+//         "font-src 'self'; " + // Salli fontit vain omasta lähteestä
+//         "connect-src 'self'; " + // Salli yhteydet omasta lähteestä
+//         "worker-src 'self';");  // Salli työntekijät omasta lähteestä
+//     next();
+// })
 
 morgan.token('data', function (req, res) {
     return JSON.stringify(req.body)

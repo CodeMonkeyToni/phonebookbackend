c05bf769b96417200fb2b08a3f36bbbd1d983763
CodeMonkeyToni <toni.koljonen@gmail.com>
2024-12-18 10:13:55 +0200

    delete added

--- index.js
+++ index.js
@@ -28,61 +28,17 @@ app.use(morgan(function (tokens, req, res) {
     return log.join(' ')
 }))
 
-// let persons = [
-//     {
-//         id: "1",
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: "2",
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: "3",
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: "4",
-//         name: "Mary Poppendieck",
-//         number: "39-23-6423122"
-//     }
-// ]
-
 app.get("/api/persons", (request, response) => {
     Person.find({}).then(result => {
         response.json(result)
     })
 })
 
-// app.get("/info", (request, response) => {
-//     const responseText = 
-//         `Phonebook has info for ${persons.length} people 
-//         ${new Date().toString()}`
-//     response.send(responseText)
-// })
-
 app.get("/api/persons/:id", (request, response) => {
     Person.findById(request.params.id).then(person => {
         response.json(person)
     })
-    // const id = request.params.id
-    // const person = persons.find(person => person.id === id)
-
-    // if (!person) {
-    //     return response.status(404).end()
-    // }
-    // response.json(person)
 })
-
-// app.delete("/api/persons/:id", (request, response) => {
-//     const id = request.params.id
-//     persons = persons.filter(person => person.id !== id)
-
-//     response.status(204).end()
-// })
 
 app.post("/api/persons", (request, response) => {
     const body = request.body
@@ -103,28 +59,13 @@ app.post("/api/persons", (request, response) => {
     person.save().then(savedPerson => {
         response.json(savedPerson)
     })
-    // const id = Math.floor(Math.random()*10000).toString()
+})
 
-    // if (!request.body.name || !request.body.number) {
-    //     return response.status(400).json({
-    //         error: "content missing"
-    //     })
-    // }
-
-    // if (persons.find(person => person.name === request.body.name))  {
-    //     return response.status(400).json({
-    //         error: "name must be unique"
-    //     })
-    // }
-
-    // const newPerson = {
-    //     id: id,
-    //     name: request.body.name,
-    //     number: request.body.number
-    // }
-    // persons.push(newPerson)
-    // console.log(persons)
-    // response.json(newPerson)
+app.delete("api/persons/:id", (request, response) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
 })
 
 const PORT = process.env.PORT || 3001
